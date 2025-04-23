eeb4611270a95efc1aba90459b0700a066b173cb
Usha-U-G <ushagondakar@gmail.com>
2025-04-23 15:16:38 +0530

    saved in master

--- calc.java
+++ calc.java
@@ -6,7 +6,12 @@ public class calc {
 
         int sum = a+b;
         int sub=a-b;
+        int mul = a*b;
+        int div=a/b;
         System.out.println("Addition of " + a + " and " + b + " is: " + sum);
         System.out.println("Subtraction of " + a + " and " + b+ " is: " + sub);
+        System.out.println("Multiplication of " + a + " and " + b + " is: " + mul);
+        System.out.println("Division of " + a + " and " + b+ " is: " + div);
+    }
     }
 }

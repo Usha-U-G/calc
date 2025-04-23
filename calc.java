3897bb2d86b16396f97b1bb33091b8b00f434a43
Usha-U-G <ushagondakar@gmail.com>
2025-04-23 15:08:52 +0530

    save

---
+++ calc.java
@@ -0,0 +1,12 @@
+public class calc {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int sum = a+b;
+        int sub=a-b;
+        System.out.println("Addition of " + a + " and " + b + " is: " + sum);
+        System.out.println("Subtraction of " + a + " and " + b+ " is: " + sub);
+    }
+}
